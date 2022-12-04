946346aa892b3376bfb55e15c55a18b4707e4389
Kyle Hernandez <kylesdev91@yahoo.com>
2022-12-04 12:36:49 -0600

    Video 4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime illo praesentium nisi laboriosam, dignissimos rem. Deserunt corporis vero, illo voluptate nobis maiores quos error ex similique mollitia consequuntur aspernatur deleniti!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, reprehenderit dolores distinctio exercitationem temporibus ex nisi, officia consectetur iure placeat cumque quae laudantium earum odio, pariatur iusto minus sit at.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
