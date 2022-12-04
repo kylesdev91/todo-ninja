56c1f7cd2d72ce0bb51237ea2e66c3f3cb4bda77
Kyle Hernandez <kylesdev91@yahoo.com>
2022-12-04 15:42:56 -0600

    Video 6

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="mx-2 pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn class="mx-2 white pink--text">click me</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
 
-    <v-btn depressed small class="mx-2 pink white--text">
-      <v-icon left small>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
 
-    <v-btn depressed large class="mx-2 pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <v-btn class="hidden-sm-only">click me</v-btn>
+
   </div>
 </template>
 
