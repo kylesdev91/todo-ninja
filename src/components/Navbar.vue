24c9a28e8a6260a6ac4822aa06ff488d671e21b3
Kyle Hernandez <kylesdev91@yahoo.com>
2022-12-27 12:36:32 -0600

    sorry guys this is the actual video 8

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,25 @@
 <template>
   <nav>
     <v-toolbar flat app>
+        <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Kyles</span>
         <span>Dev</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
+    <v-navigation-drawer v-model="drawer" temporary app class="indigo">
+          <p>test</p>
+</v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            drawer: false
+        }
+    }
+};
 </script>
