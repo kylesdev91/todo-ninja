0defbdcbc63fb0e948af2d94ccdcd81179e07d56
Kyle Hernandez <kylesdev91@yahoo.com>
2022-12-27 20:53:21 -0600

    this is actually videos 9 and 10

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,51 @@
 <template>
   <nav>
     <v-toolbar flat app>
-        <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Kyles</span>
         <span>Dev</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
-    <v-navigation-drawer v-model="drawer" temporary app class="indigo">
-          <p>test</p>
-</v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer" class="primary">
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{
+              link.text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            drawer: false
-        }
-    }
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" },
+      ],
+    };
+  },
 };
 </script>
+
+<style></style>
